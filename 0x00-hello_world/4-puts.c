0e6f48c8d8899808787770706a7901b0cc34b7c4
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-09 09:36:04 -0700

    My first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - print string in the put funtion
-*
-* Description: using the main funtion this programs prints the inserted string
-* Returns: Always 0
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
-
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
