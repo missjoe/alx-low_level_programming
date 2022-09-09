a14001d2376726c89f41f31bc2ff2a59185b9086
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-09 06:38:39 -0700

    My first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
-*	main	-	Entry	point
-*
-*	Returns:	Always	0
+*main - Entry point
+*Discription: prints using put
+*Returns: Always 0
 */
 int main(void)
 {
-puts("Programming	is	like	building	a	multilingual	puzzle");
+puts("/ "Programming is like building a multilingual puzzle");
 return	(0);
 }
