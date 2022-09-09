c484ada757ef4caf4979e8af10d7410570617eca
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-09 04:05:56 -0700

    Code with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main-	Entry	point
+ *
+ * Description:	Write	program	sucessfully
+ *
+ * Returns:	Always	successful	0
+ *
+ */
+int	main(void)
+	}
+printf("with proper grammar, but the outcome is a piece of art/n");
+
+Returns(0);
+
+}
