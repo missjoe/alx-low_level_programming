17143bcf47d4f28b201686e532bed3e67607a51b
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-09 05:54:34 -0700

    My first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
 *
 *	Returns:	Always	0
 */
-int	main(void)
+int main(void)
 {
-puts(""Programming is like building a multilingual puzzle");
+puts("Programming	is	like	building	a	multilingual	puzzle");
 return	(0);
 }
