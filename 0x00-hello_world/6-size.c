2f7dab3b70abaa9a70c658c942a4898970fc2202
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-10 13:47:23 -0700

    Sizes of computers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ char d;
 float f;
 printf("size of a char: %d byte(s)\n", (unsigned long)sizeof(d));
 printf("size of an int: %d byte(s)\n", (unsigned long)sizeof(a));
-printf("size of a long int: %d bit\n", (unsigned long)sizeof(b));
-printf("size of a long long int: %d bit\n", (unsigned long)sizeof(c));
+printf("size of a long int: %d byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long long int: %d byte(s)\n", (unsigned long)sizeof(c));
 printf("size of a float: %d byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
