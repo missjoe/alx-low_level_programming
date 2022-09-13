a218f7cfb54705b2a11bdddfe373129004725f83
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-13 13:13:00 -0700

    Header file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Prints alphabets in lower case.
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	for (i = 'a'; i < = 'z'\n"; i++)
+		putchar(" % c", i);
+	{
+		return (0);
+	}
