efedfb9fd23d6172826979fe9532526632e0ae5f
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-13 16:32:39 -0700

    Using putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - Prints alphabets in lower case.
+ * print_alphabet - This is Entry
  *
- * Return: Always 0 (sucess)
+ * Return: 0
  */
 void print_alphabet(void)
 {
