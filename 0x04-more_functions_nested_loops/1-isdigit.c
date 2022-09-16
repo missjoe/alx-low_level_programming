a79c961eaccf73e08967bf29f675479147e42626
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-16 09:47:03 -0700

    More nested functions 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 /**
  * _isdigit - checks whether a number passed as an argument is digit number
  * @c: argument to be checked
- * Return: returns 1 if sucessful or 0 if otherwise
+ * Return: returns 1 if sucessful, 0 otherwise
  */
 int _isdigit(int c)
 {
 	if ((c >= 48) && (c <= 57))
 	{
 		return (1);
-	else
-		return (0);
 	}
+	return (0);
+}
