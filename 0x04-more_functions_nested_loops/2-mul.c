a96e46dce4845e295b6fb283c267af554e5df02a
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-16 10:18:40 -0700

    More function 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - This is the function name
+ * @a: the int to be multiplied to b
+ * @b: the int to be multiplied to a
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
