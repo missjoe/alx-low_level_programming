e3f3752b2a1990bc116ab044eb0971657a571823
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-16 15:01:52 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - This the function name
+ * @n: This is the funtion parameter
+ * Return: 0
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
