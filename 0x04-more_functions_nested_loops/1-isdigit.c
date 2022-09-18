47a49fb9fdb188ec25be2c340f8eb878b0850211
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-18 09:36:44 -0700

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * _isdigit - checks whether a number passed as an argument is digit number
- * @c: argument to be checked
- * Return: returns 1 if sucessful, 0 otherwise
+ * _isdigit - This is the function name
+ * @c: the int to be checked
+ * Description: function that checks for a digit (0 through 9).
+ * Return: 1 if c is uppercase or 0 if otherwise
  */
 int _isdigit(int c)
 {
