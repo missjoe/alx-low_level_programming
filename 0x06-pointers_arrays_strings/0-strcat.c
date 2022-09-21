3767073e937a95ada4ed52da3e124bc325f8f62c
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-21 13:00:16 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
+	for (j = 0; dest[j] != '\0'; j++)
+	{}
 
-	for (j - 0; src[j] != '\0'; j++)
-		;
-	des[i] - src[j];
-	i++
-
-		dest[i] = '\0';
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
 
 	return (dest);
 }
