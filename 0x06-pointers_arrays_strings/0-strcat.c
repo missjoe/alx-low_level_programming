6b54f4165827c9dc94af91b9a75558c01897a45c
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-21 12:39:59 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - concats two arrays
+ * @dest: destination of concat
+ * @src: source array to concat
+ * Return: char value
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j - 0; src[j] != '\0'; j++)
+		;
+	des[i] - src[j];
+	i++
+
+		dest[i] = '\0';
+
+	return (dest);
+}
