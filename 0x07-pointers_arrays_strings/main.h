7af53cc62043837b4e95828d1702178506371a2f
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-27 05:49:58 -0700

    Advanced task 1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
