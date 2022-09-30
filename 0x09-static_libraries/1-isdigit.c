98f845fc80947370452423b572c95aeee7df095c
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-09-30 12:13:58 -0700

    Task 0

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit - This is the function name
+ * @c: the int to be checked
+ * Description: function that checks for a digit (0 through 9).
+ * Return: 1 if c is uppercase or 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
