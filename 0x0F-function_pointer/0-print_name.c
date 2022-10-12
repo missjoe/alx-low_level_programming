e7db9656baf4f9bb1ce0d6dafd1704a53f8ce95b
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-10-12 14:10:49 -0700

    Task 0

--- 0x0F-function_pointer/0-print_name.c
+++ 0x0F-function_pointer/0-print_name.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "function_pointers.h"
+
 /**
  * print_name - print the name
  * @name: name to print
  * @f: function to use
+ *
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name && f)
 		f(name);
 }
