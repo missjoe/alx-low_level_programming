9707d343b195f58d3f75ab6e5ba83662b277b074
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-10-13 19:50:09 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_function.h"
+#include "variadic_functions.h"
 /**
  * sum_them_all - sum all args
  * @n: integers to sum
