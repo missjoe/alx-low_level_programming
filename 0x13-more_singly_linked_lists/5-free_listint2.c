c8d56c78660a2419b0672929b9c504233d030d6b
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-10-24 15:26:50 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list but head is null
+ * @head: linked list and head
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(*head);
+	}
+	*head = NULL;
+}
