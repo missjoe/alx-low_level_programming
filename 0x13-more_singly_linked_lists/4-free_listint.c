1cba8fabc8cb9fb85c0077fa559b17883054591a
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-10-24 15:16:39 -0700

    Task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,16 +3,17 @@
  * free_listint - frees the list
  * @head: head of struct
  *
+ * Return: void
  */
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
 	while (head != NULL)
 	{
-		tmp = head;
-		free(head);
-		head = tmp->next;
+		temp = head;
+		head = head->next;
+		free(temp);
 	}
 
 }
