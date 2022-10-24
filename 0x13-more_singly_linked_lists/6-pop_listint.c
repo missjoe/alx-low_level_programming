86ff59bcf18d62da9cb010c4b9a0cb2efa6d3629
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-10-24 15:54:44 -0700

    Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,7 @@
  * pop_listint - deletes the head node
  * @head: pointer to the head of the linked list
  * Return: head node's data (n)
- * 	or 0 - if the linked list is empty
+ * or 0 - if the linked list is empty
  */
 int pop_listint(listint_t **head)
 {
