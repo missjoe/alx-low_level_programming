90e3107830473f74a3a77670efdbe3d4a55db092
John Tochi Josephine <johntochijosephine1995@gmail.com>
2022-10-25 14:22:19 -0700

    Advanced task 2

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -27,7 +27,7 @@ void free_listp(listp_t **head)
  * print_listint_safe - prints a linked list.
  * @head: head of list.
  *
- * Return number of nodes in the list.
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
